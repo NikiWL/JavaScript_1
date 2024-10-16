66b98e4939b28125e1af35bb292f2e5d1b9bd973
NikiWL <9027464@student.zadkine.nl>
2024-10-16 10:42:30 +0200

    opdracht 3 & 4

---
+++ opdracht_3 & 4/app.js
@@ -0,0 +1,21 @@
+let heading = document.getElementById('text')
+
+function changeText() {
+    heading.style.color = 'orange';
+    heading.innerText = 'Hallo';
+}
+
+function changeText2() {
+    heading.style.color = 'blue';
+    heading.innerText = 'Hallo';
+}
+
+function changeText3() {
+    heading.style.color = 'yellow';
+    heading.innerText = 'Hallo';
+}
+
+function changeText4() {
+    heading.style.color = 'red';
+    heading.innerText = 'Hallo';
+}
