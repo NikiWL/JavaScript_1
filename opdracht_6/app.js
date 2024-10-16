6bedb3afc659335b9f4a4aaa2934c88c7b8d0c66
NikiWL <9027464@student.zadkine.nl>
2024-10-16 11:42:52 +0200

    opdracht 6

---
+++ opdracht_6/app.js
@@ -0,0 +1,13 @@
+console.log()
+
+function calculate(parameter1, parameter2, parameter3) {
+    calculate(24,25)
+}
+
+function multiplyByFive(parameter1, parameter2, parameter3) {
+    multiplyByFive(5)
+}
+
+function calculateMinutes(parameter1, parameter2, parameter3) {
+    calculateMinutes(2400)
+}
