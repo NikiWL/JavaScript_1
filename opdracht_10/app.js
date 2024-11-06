71c7ba012b38ceb15ffb935eeb6bff84605c1885
NikiWL <9027464@student.zadkine.nl>
2024-11-06 11:49:23 +0100

    opdracht 10

---
+++ opdracht_10/app.js
@@ -0,0 +1,24 @@
+let answer = document.getElementById("answer");
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2")
+
+function multiply(){
+ let ans = num1.value * num2.value;
+ answer.innerText = ans
+
+}
+function devide(){
+ let ans = num1.value / num2.value;
+ answer.innerText = ans
+
+}
+function add(){
+ let ans = num1.value + num2.value;
+ answer.innerText = ans
+
+}
+function minus(){
+ let ans = num1.value - num2.value;
+ answer.innerText = ans
+
+}
