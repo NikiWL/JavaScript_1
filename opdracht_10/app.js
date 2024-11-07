a02566aa08b5deddc79b4b11c87d9f3a311f4eb6
NikiWL <9027464@student.zadkine.nl>
2024-11-07 10:05:38 +0100

    Update app.js

--- opdracht_10/app.js
+++ opdracht_10/app.js
@@ -3,22 +3,40 @@ let num1 = document.getElementById("num1");
 let num2 = document.getElementById("num2")
 
 function multiply(){
- let ans = num1.value * num2.value;
- answer.innerText = ans
+    if(num1.value > 0 && num2.value > 0){
+        let ans = num1.value * num2.value;
+        answer.innerText = ans
+    }else {
+        answer.innerText = "het getal moet hoger zijn dan 0"
+    }
+ 
 
 }
 function devide(){
- let ans = num1.value / num2.value;
- answer.innerText = ans
-
+    if(num1.value > 0 && num2.value > 0){
+        let ans = num1.value / num2.value;
+        answer.innerText = ans
+    }else {
+        answer.innerText = "het getal moet hoger zijn dan 0"
+    }
 }
+
+
 function add(){
- let ans = num1.value + num2.value;
- answer.innerText = ans
-
+ if(num1.value > 0 && num2.value > 0){
+        let ans = Number(num1.value) + Number(num2.value)
+        answer.innerText = ans
+    }else {
+        answer.innerText = "het getal moet hoger zijn dan 0"
+    }
 }
-function minus(){
- let ans = num1.value - num2.value;
- answer.innerText = ans
 
+
+function minus(){
+ if(num1.value > 0 && num2.value > 0){
+        let ans = num1.value - num2.value;
+        answer.innerText = ans
+    }else {
+        answer.innerText = "het getal moet hoger zijn dan 0"
+    }
 }
